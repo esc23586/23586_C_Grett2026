c12669a2ec92958c400e8d407bfdaa46948796c1
esc23586 <esc23586@uvg.edu.gt>
2026-04-07 13:37:58 -0600

    Contador

--- Prelab4/Prelab4/main.c
+++ Prelab4/Prelab4/main.c
@@ -20,19 +20,64 @@ Contador:
 /****************************************/
 // Encabezado (Libraries)
 #include <avr/io.h>
-#include <avr/interrupt.h>
+//#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
 /****************************************/
 // Function prototypes
 /****************************************/
 // Main Function
-int main (void)
+int main(void)
 {
-	cli(); //deshabilitar interrupciones
-	while(1)
+	//******** Configuración salidas***********
+	DDRB = 0x3F;   // PB0–PB5
+	DDRC |= 0x03;  // PC0–PC1
+
+	//********* Configuración entradas**************
+	DDRC &= ~((1<<PC2) | (1<<PC3)); // PC2, PC3 entradas
+	PORTC |= (1<<PC2) | (1<<PC3);   // Pull-ups
+
+	uint8_t contador = 0; //igualo mi contador a 0 
+
+	while (1)
 	{
-		
+		// BOTÓN UP (PC2)
+		if (!(PINC & (1<<PC2)))  // Presionado (activo en 0)
+		{
+			_delay_ms(20); // antirrebote
+
+			if (!(PINC & (1<<PC2))) // confirmar
+			{
+				contador++; // overflow automático
+
+				// Esperar a que se suelte
+				while (!(PINC & (1<<PC2)));
+			}
+		}
+
+		// BOTÓN DOWN (PC3)
+		if (!(PINC & (1<<PC3)))
+		{
+			_delay_ms(20);
+
+			if (!(PINC & (1<<PC3)))
+			{
+				contador--; // underflow automático
+
+				while (!(PINC & (1<<PC3)));
+			}
+		}
+
+		//  Mostrar en LEDs
+
+		// Bits 0–5 ? PORTB
+		PORTB = contador & 0x3F;
+
+		// Bits 6–7 ? PC0–PC1
+		PORTC = (PORTC & 0xFC) | ((contador >> 6) & 0x03);
 	}
 }
+
 /****************************************/
 // NON-Interrupt subroutines
 /****************************************/
