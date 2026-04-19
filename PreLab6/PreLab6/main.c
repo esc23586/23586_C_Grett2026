a097ea61c3d40da7e7ab8d8eee19b960376ecb6c
esc23586 <esc23586@uvg.edu.gt>
2026-04-18 19:33:53 -0600

    Creación del documento prelab 6

---
+++ PreLab6/PreLab6/main.c
@@ -0,0 +1,18 @@
+/*
+ * PreLab6.c
+ *
+ * Created: 18/04/2026 19:32:18
+ * Author : grett
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
